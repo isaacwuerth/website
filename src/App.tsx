adc280b6b90bc6362bf7432184b972f2bc64184a
cutmal <isaac.wuerth@outlook.com>
2022-08-08 11:04:23 +0200

    chore: add inital files

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,56 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.sass'
+import { Helmet } from 'react-helmet'
+import { Link, Outlet } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="app">
-      <header className="app-header">
-
-      </header>
-      <footer className="app-footer">
-
-      </footer>
-    </div>
-  );
+type navitem = {
+  title: string
+  path: string
 }
 
-export default App;
+function App() {
+  const title = 'Isaac Würth'
+  const [darkMode, setDarkmode] = useState<boolean>(true)
+
+  const navigation: navitem[] = [
+    { title: 'About', path: '/about' },
+    { title: 'Projects', path: '/projects' },
+    { title: 'Blog', path: '/blog' },
+  ]
+
+  useEffect(() => {
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme:dark)').matches
+    setDarkmode(prefersDarkMode)
+  }, [])
+
+  function toggleButton() {
+    setDarkmode(!darkMode)
+  }
+
+  return (
+    <div className='app'>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      <header className='app-header blur'>
+        <div className='app-header-logo'>IW</div>
+        <ul id='app-nav'>
+          {navigation.map((value) => (
+            <li>
+              <Link to={value.path}>{value.title}</Link>
+            </li>
+          ))}
+        </ul>
+        <button type='button' id='theme-toggle' onClick={toggleButton} className={darkMode ? 'theme-toggle-moon' : ''}>
+          <div id='theme-toggle-shape' className={darkMode ? 'theme-toggle-moon-shape' : 'theme-toggle-sun'} />
+        </button>
+      </header>
+      <div className='app-content blur'>
+        <Outlet />
+      </div>
+      <footer className='app-footer blur' />
+    </div>
+  )
+}
+
+export default App
